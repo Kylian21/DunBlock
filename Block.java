52f97d39f56aeda3c25bebe7dc8bb72bb2ec9b04
Louis HARISMENDY <louis.harismendy@gmail.com>
2019-10-07 17:06:45 +0200

    Block/Full Block/MineralBlock

---
+++ Block.java
@@ -0,0 +1,17 @@
+/*
+ * DunBlock JAVA project
+ */
+
+/**
+ *
+ * @author Louis HARISMENDY & Kylian SALOMON
+ */
+public class Block {
+    private final Point position=new Point();
+    private boolean canBeTravel;
+    private int generationRate;
+    
+    public Point getPosition() {
+        return position;
+    }
+}
